50f649e128ab666b2cd15abc31ad3cf48b6defec
Priscila Finkler <prifinkler@gmail.com>
2023-08-04 19:11:04 +0100

    fixed review destroy action

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
+  validates :rating,
+            inclusion: { in: 0..5, message: 'Should be between 0 to 5' },
+            numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: [:new]
+    resources :reviews, only: %i[index new create]
   end
-
+  resources :reviews, only: %i[destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
