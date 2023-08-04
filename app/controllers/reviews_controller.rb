f09439ab38500fd8e6cc8a8a078dfb05217eadfc
Priscila Finkler <prifinkler@gmail.com>
2023-08-04 19:28:58 +0100

    fixed added new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
@@ -30,6 +30,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: %i[index new create]
+    resources :reviews, only: %i[index show new create]
   end
   resources :reviews, only: %i[destroy]
   # Defines the root path route ("/")
